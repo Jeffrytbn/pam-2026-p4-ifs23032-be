9eef1c0aa6ef6be51aee1f734d83007f0615814e
Jeffrytbn <jeffrytbn1206@gmail.com>
2026-03-02 15:18:51 +0700

    init

--- src/main/kotlin/module/AppModule.kt
+++ src/main/kotlin/module/AppModule.kt
@@ -2,23 +2,40 @@ package org.delcom.module
 
 import org.delcom.repositories.IPlantRepository
 import org.delcom.repositories.PlantRepository
+import org.delcom.repositories.IRumahAdatRepository
+import org.delcom.repositories.RumahAdatRepository
 import org.delcom.services.PlantService
 import org.delcom.services.ProfileService
+import org.delcom.services.RumahAdatService
 import org.koin.dsl.module
 
-
 val appModule = module {
-    // Plant Repository
+
+    // =========================
+    // PLANT
+    // =========================
     single<IPlantRepository> {
         PlantRepository()
     }
 
-    // Plant Service
     single {
         PlantService(get())
     }
 
-    // Profile Service
+    // =========================
+    // RUMAH ADAT
+    // =========================
+    single<IRumahAdatRepository> {
+        RumahAdatRepository()
+    }
+
+    single {
+        RumahAdatService(get())
+    }
+
+    // =========================
+    // PROFILE
+    // =========================
     single {
         ProfileService()
     }
