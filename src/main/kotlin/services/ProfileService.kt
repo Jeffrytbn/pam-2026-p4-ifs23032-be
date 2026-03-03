5a6d951e6370bd70368a2c0b3b00f28b63b023fe
Jeffrytbn <jeffrytbn1206@gmail.com>
2026-03-03 22:01:54 +0700

    init

--- src/main/kotlin/services/ProfileService.kt
+++ src/main/kotlin/services/ProfileService.kt
@@ -13,8 +13,8 @@ class ProfileService {
             "success",
             "Berhasil mengambil profile pengembang",
             mapOf(
-                Pair("username", "abdullah.ubaid"),
-                Pair("nama", "Abdullah Ubaid"),
+                Pair("username", "Jeffry Tambunan"),
+                Pair("nama", "Jeffrytbn"),
                 Pair("tentang", "Saya adalah seorang developer yang tertarik pada mobile development, backend API, dan berbagai teknologi pengembangan aplikasi. Senang belajar hal baru dan membangun aplikasi yang berguna."),
             )
         )
